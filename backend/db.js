f3b1d7d3bfe1d54cd3b779c22f47239b3895ad1e
Yagnesh <20bsm060@iiitdmj.ac.in>
2023-09-21 02:32:56 +0530

    second commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-const mongoURI = "mongodb+srv://chetanpanditt48:sahayata@cluster0.krp9ax1.mongodb.net/inotebookdata?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://20bsm060:YagneshParth@yagneshdatabase.5hlpou4.mongodb.net/Doc-app"
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log(`Connected to Mongo Successfully ${mongoose.connection.host}`);
